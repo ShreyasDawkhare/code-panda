78f7289c8608b4b53c93b31fe21782004b066e1c
Shreyas Dawkhare <shreyas.dawkhare@walmart.com>
2021-01-26 13:20:16 +0530

    initial commit

---
+++ src/main/java/com/dawkhare/shreyas/codepanda/CodePandaApplication.java
@@ -0,0 +1,13 @@
+package com.dawkhare.shreyas.codepanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawkhare/shreyas/codepanda/CodePandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawkhare.shreyas.codepanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
